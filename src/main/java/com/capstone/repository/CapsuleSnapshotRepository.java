f4a8f183da1b1c6eecf3a6a20ff631450265ada5
kevin <murindakevinmulk@gmail.com>
2025-09-30 18:41:59 +0200

    feat: add derived query methods

--- src/main/java/com/capstone/repository/CapsuleSnapshotRepository.java
+++ src/main/java/com/capstone/repository/CapsuleSnapshotRepository.java
@@ -4,8 +4,10 @@ import com.capstone.model.CapsuleSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CapsuleSnapshotRepository extends JpaRepository<CapsuleSnapshot, UUID> {
+    Optional<CapsuleSnapshot> findByCapsuleId(UUID capsuleId);
 }
