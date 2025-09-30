8956f1c284a8db70af229e27527531aa4a1afdf6
kevin <murindakevinmulk@gmail.com>
2025-09-30 18:44:47 +0200

    feat: create mapper class for mapping kafka events to entity classes

---
+++ src/main/java/com/capstone/mapper/CapsuleEventMapper.java
@@ -0,0 +1,28 @@
+package com.capstone.mapper;
+
+import com.capstone.model.CapsuleSnapshot;
+import org.common.event.SkillCapsuleEvent;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface CapsuleEventMapper {
+    @Mapping(source = "id", target = "capsuleId")
+    @Mapping(source = "name", target = "capsuleName")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "difficultyLevel", source = "difficulty")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "issuedBadges", ignore = true)
+    CapsuleSnapshot toCapsuleSnapshot(SkillCapsuleEvent event);
+
+    @Mapping(source = "name", target = "capsuleName")
+    @Mapping(target = "difficultyLevel", source = "difficulty")
+    @Mapping(target = "capsuleId", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "issuedBadges", ignore = true)
+    void updateCapsuleSnapshot(SkillCapsuleEvent event, @MappingTarget CapsuleSnapshot capsuleSnapshot);
+}
---
+++ src/main/java/com/capstone/mapper/UserEventMapper.java
@@ -0,0 +1,25 @@
+package com.capstone.mapper;
+
+import com.capstone.model.UserSnapshot;
+import org.common.event.ProduceUserEvent;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface UserEventMapper {
+
+    @Mapping(source = "versapathUserId", target = "userId")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "issuedBadges", ignore = true)
+    UserSnapshot toUserSnapshot(ProduceUserEvent event);
+
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "issuedBadges", ignore = true)
+    void updateUserSnapshot(ProduceUserEvent event, @MappingTarget UserSnapshot userSnapshot);
+}
