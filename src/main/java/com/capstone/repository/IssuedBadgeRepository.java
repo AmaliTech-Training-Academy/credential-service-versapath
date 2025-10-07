403cf984930ce8b05feab6db146cedce8a62cd4d
kevin <murindakevinmulk@gmail.com>
2025-10-07 09:28:22 +0200

    feat: add derived query methods for database transaction

--- src/main/java/com/capstone/repository/IssuedBadgeRepository.java
+++ src/main/java/com/capstone/repository/IssuedBadgeRepository.java
@@ -4,8 +4,11 @@ import com.capstone.model.IssuedBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IssuedBadgeRepository extends JpaRepository<IssuedBadge, UUID> {
+    boolean existsByUserSnapshotUserIdAndCapsuleSnapshotCapsuleId(UUID userId, UUID capsuleId);
+    Optional<IssuedBadge> findByUserSnapshotUserIdAndCapsuleSnapshotCapsuleId(UUID userId, UUID capsuleId);
 }
