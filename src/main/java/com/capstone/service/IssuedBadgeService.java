2cace39b90792c0012cbafa83eb73318640e66ed
Kevin_Murinda <162316622+Ganza-Kevin-Murinda@users.noreply.github.com>
2025-10-08 11:09:40 +0200

    Revert "Feat: Badge Endpoints"

--- src/main/java/com/capstone/service/IssuedBadgeService.java
+++ src/main/java/com/capstone/service/IssuedBadgeService.java
@@ -1,19 +1,11 @@
 package com.capstone.service;
 
-import com.capstone.dto.response.LearnerBadgeResponseDto;
-import com.capstone.dto.response.PaginatedResponseDto;
 import com.capstone.model.IssuedBadge;
 import org.common.event.CapsuleCompletionEvent;
-import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface IssuedBadgeService {
     IssuedBadge processCapsuleCompletionEvent(CapsuleCompletionEvent event);
     boolean isAlreadyIssued(UUID learnerId, UUID capsuleId);
-
-    PaginatedResponseDto<LearnerBadgeResponseDto> getLearnerBadges(UUID learnerId, Pageable pageable);
-    Optional<LearnerBadgeResponseDto> getBadgeByVerificationCode(String verificationCode);
-    long countLearnerBadges(UUID learnerId);
 }
